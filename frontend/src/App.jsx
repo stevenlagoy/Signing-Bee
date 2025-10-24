2e9cd5e15cdf0464797903bca1788f9dbc364e2e
stevenlagoy <stevenlagoy@gmail.com>
2025-10-24 10:50:57 -0400

    Add react-router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import ASLReference from "./pages/ASLReference";
+import Learn from "./pages/Practice";
+import Profile from "./pages/Profile";
+import NotFound from "./pages/NotFound";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import styles from "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="min-h-screen bg-white">
-      <Header />
-      {/* Rest of the site */}
-    </div>
+    <BrowserRouter>
+      <div className="screen">
+        <Header />
+        <main className={styles.main}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/asl-reference" element={<ASLReference />} />
+            <Route path="/practice" element={<Learn />} />
+            {/* Consider using those dynamic page names we talked about on the 15th for user profiles. */}
+            <Route path="/profile" element={<Profile />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
-
-export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from "./App"
 
 // eslint-disable-next-line no-undef
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
