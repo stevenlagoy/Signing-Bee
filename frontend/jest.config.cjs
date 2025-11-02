acd76471a5dc0e4af9c7476ba8fddbe18bee6b8e
stevenlagoy <stevenlagoy@gmail.com>
2025-11-02 13:08:05 -0500

    Jest config update

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -1,10 +1,16 @@
 module.exports = {
   testEnvironment: "jsdom",
   setupFiles: ["<rootDir>/jest.setup.js"],
-  transform: { "^.+\\.[jt]sx?$": "babel-jest" },
+  transform: { 
+    "^.+\\.[jt]sx?$": "babel-jest" 
+  },
   moduleFileExtensions: ["js", "jsx"],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
-    "\\.(css|scss|sass)$": "identity-obj-proxy"
-  }
+    "\\.(css|scss|sass)$": "identity-obj-proxy",
+    "\\.(jpg|jpeg|png|gif|svg)$": "<rootDir>/__mocks__/fileMock.js"
+  },
+  transformIgnorePatterns: [
+    "node_modules/(?!(your-esm-package)/)"
+  ]
 };
