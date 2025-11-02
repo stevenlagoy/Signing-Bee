16a6fc57fd786ea95fc8782c22378671559fdde6
Zachary McGill <zmcgill3@ivytech.edu>
2025-11-01 20:17:05 -0400

    Fixed and organized our testing setup. I also upgraded our outdated testing packages which were deprecated from 2022. This required me to implement a few fixes but I was able to run the jest test and get it worked as well as verify cypress although I didn't run a test with it. I moved all of the root  testing files into frontend so we had clear speraration of frontend and backend testing in order to follow best practices.

--- frontend/cypress/e2e/homepage.cy.js
+++ frontend/cypress/e2e/homepage.cy.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-undef */
 describe('template spec', () => {
   it("page loads", () => {
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:5173')
   })
   it('header text', () => {
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:5173')
     cy.get("h1").contains("Signing Bee")
   })
 })
--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -6,6 +6,6 @@ import App from './App';
 
 test('renders header with site name', () => {
   render(<App />);
-  const headerElement = screen.getByText(/Signing Bee/i);
+  const headerElement = screen.getByText(/^Signing Bee$/i); // Test was failing due to finding two mataches instead of one so I tightened the regex
   expect(headerElement).toBeInTheDocument();
 });
