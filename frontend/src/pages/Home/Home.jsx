f4ce65ee9ca090d5b883deda95e90ed815b66baf
stevenlagoy <stevenlagoy@gmail.com>
2025-11-24 16:45:32 -0500

    Make the home and about pages prettier with styling and new images and content

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,28 +1,43 @@
+import styles from "./Home.module.scss";
 import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Home() {
     return (
         <>
-            <section>
-                <h1>Welcome to Signing Bee!</h1>
-                <h2>Why learn ASL?</h2>
-                <p>
-                    Up to 2 million Americans use sign language daily as their primary language, but
-                    less than 3% of people in the US can understand ASL. On this site, you can play
-                    interactive games which will teach the basics of ASL. You will learn and then
-                    practice ASL signs and earn points for accurate, quick signing. You can also
-                    compare your performance with other players on the leaderboard.
-                </p>
-                <h2>How to Play</h2>
-                <p>
-                    There are several ways to learn ASL on Signing Bee!
-                </p>
-
-                <h2>Ready to Play?</h2>
-                <Link to="/practice">
-                    <p>Click here to enter Practice Mode</p>
-                </Link>
+            <section className={styles.home}>
+                <h1>Welcome to Signing Bee!<img src="signing-bee-logo.svg" width="50" height="50" /></h1>
+                <div>
+                    <h2>Why learn ASL?</h2>
+                    <p>
+                        Up to 2 million Americans use sign language daily as their primary language, but
+                        less than 3% of people in the US can understand ASL. On this site, you can play
+                        interactive games which will teach the basics of ASL. You will learn and then
+                        practice ASL signs and earn points for accurate, quick signing. You can also
+                        compare your performance with other players on the leaderboard.
+                    </p>
+                </div>
+                <div>
+                    <h2>How to Play</h2>
+                    <p>
+                        There are several ways to learn ASL on Signing Bee! Start by going to
+                        the <Link to="/asl-reference">ASL Reference page</Link> and learn to sign the
+                        alphabet. You can turn on your camera to test your signing. Once you are
+                        ready, go to the <Link to="/practice">Practice page</Link> and test out
+                        signing letters to make words. 
+                    </p>
+                </div>
+                <div>
+                    <h2>Ready to Sign?</h2>
+                    <Link to="/practice">
+                        <button>Click here to enter Practice Mode</button>
+                    </Link>
+                </div>
+                <div className={styles.images}>
+                    <img src="signing2.png" alt="Close-up on womans hands showing sign language gesture with two index fingers pointing away from the signer." width="450" height="300" />
+                    <img src="signing1.png" alt="Close-up on womans hand showing sign language gesture for &quot;I love you.&quot;" width="200" height="300" />
+                    <img src="signing3.png" alt="Close-up on womans hands showing sign language gesture with cupped hands." width="450" height="300" />
+                </div>
             </section>
         </>
     );
