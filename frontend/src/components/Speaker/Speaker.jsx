5e5fd5668f33f1eaf1b87ee77debfc5a340de310
Ricardo Saldana <rsald4960@gmail.com>
2025-12-04 10:10:57 -0500

    speaker and score commit

---
+++ frontend/src/components/Speaker/Speaker.jsx
@@ -0,0 +1,32 @@
+import styles from "./speaker.module.scss";
+
+export default function Speaker(props) {
+    const muted = props.muted;
+    const setMuted = props.setMuted;
+
+    function handleToggle() {
+        setMuted(!muted);
+    }
+
+    let buttonClass = styles.muteBtn;
+    if (muted) {
+        buttonClass = buttonClass + " " + styles.muted;
+    }
+
+    let imgSrc;
+    let imgAlt;
+
+    if (muted) {
+        imgSrc = "/speaker-off.svg";
+        imgAlt = "Unmute audio";
+    } else {
+        imgSrc = "/speaker-on.svg";
+        imgAlt = "Mute audio";
+    }
+
+    return (
+        <button className={buttonClass} onClick={handleToggle}>
+            <img src={imgSrc} alt={imgAlt} />
+        </button>
+    );
+}
