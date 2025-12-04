8740aaa3dd11b5f90821dd72ddcc836f276e2aa5
stevenlagoy <stevenlagoy@gmail.com>
2025-12-03 22:35:16 -0500

    Use environment variables to route database requests to production database. Need to set VITE_API_BASE in Google Cloud environment variables to complete

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -5,10 +5,6 @@ dotenv.config();
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  connectionString: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
   ssl: { rejectUnauthorized: false },
 });
--- frontend/src/components/Leaderboard/Leaderboard.jsx
+++ frontend/src/components/Leaderboard/Leaderboard.jsx
@@ -7,9 +7,11 @@ export default function Leaderboard() {
     const [usersScores, setUsersScores] = useState([]);
     const numberLeaderboardEntries = 5;
 
+    const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:4000';
+
     useEffect(() => {
         const getUsers = async () => {
-            const res = await fetch("http://localhost:4000/users");
+            const res = await fetch(`${API_BASE}/users`);
             if (!res.ok) {
                 throw new Error(`Error fetching leaderboard: ${res.status}`);
             }
